503eb6dbe6b1c6ae332f5462c010f2794f7a2b49
tinh <tinh101172@gmail.com>
2024-02-27 14:46:59 +0900

    new

---
+++ app/src/main/java/com/example/actestapp/MainActivity.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.actestapp;
+
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * The TwoActivities app contains two activities and sends messages (intents) between them.
+ */
+public class MainActivity extends AppCompatActivity {
+    // Class name for Log tag
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    // Unique tag required for the intent extra
+    public static final String EXTRA_MESSAGE = "com.example.actestapp.extra.MESSAGE";
+    // Unique tag for the intent reply
+    public static final int TEXT_REQUEST = 1;
+
+    // EditText view for the message
+    private EditText mMessageEditText;
+    // TextView for the reply header
+    private TextView mReplyHeadTextView;
+    // TextView for the reply body
+    private TextView mReplyTextView;
+    //Button
+    private Button mButton;
+    /**
+     * Initializes the activity.
+     *
+     * @param_savedInstanceState The current state data.
+     *
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // If the heading is visible, message needs to be saved.
+        // Otherwise we're still using default layout.
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text",
+                    mReplyTextView.getText().toString());
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
+        // Initialize all the view variables.
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+        // Restore the saved state.
+        // See onSaveInstanceState() for what gets saved.
+        if (savedInstanceState != null) {
+            boolean isVisible =
+                    savedInstanceState.getBoolean("reply_visible");
+            // Show both the header and the message views. If isVisible is
+            // false or missing from the bundle, use the default layout.
+            if (isVisible) {
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState
+                        .getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+
+        mButton = findViewById(R.id.button_main);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchSecondActivity(view);
+            }
+        });
+
+
+
+    }
+
+    /**
+     * Handle the onClick for the "Send" button. Gets the value of the main EditText,
+     * creates an intent, and launches the second activity with that intent.
+     *
+     * The return intent from the second activity is onActivityResult().
+     * @param_view The view (Button) that was clicked.
+     */
+
+    public void launchSecondActivity(View view) {
+        Intent explicitIntent = new Intent(this, SecondActivity.class);
+        explicitIntent.putExtra(EXTRA_MESSAGE, mMessageEditText.getText().toString());
+        Log.d(LOG_TAG, "Intent.outExtra");
+        startActivityForResult(explicitIntent, TEXT_REQUEST);
+        Log.d(LOG_TAG, "Button clicked!");
+    }
+
+
+    /**
+     * Handle the data in the return intent from SecondActivity.
+     *
+     * @param requestCode Code for the SecondActivity request.
+     * @param resultCode Code that comes back from SecondActivity.
+     * @param data Intent data sent back from SecondActivity.
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Test for the right intent reply
+        if (requestCode == TEXT_REQUEST) {
+            // Test to make sure the intent reply result was good.
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                // Make the reply head visible.
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+                // Set the reply and make it visible.
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+}
